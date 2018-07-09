69fa97721a1793b7bdb9f9efa92788e39504e750
Nampy <at_ben@encs.concordia.ca>
2018-07-08 23:26:48 -0400

    implemented getters

--- src/com/company/Block.java
+++ src/com/company/Block.java
@@ -4,9 +4,29 @@ public class Block {
     private int width;
     private int height;
     private int length;
-    public Block(int[] a){
-        // constructor
-        this.width = a[0];
 
+    public Block(int[] a){
+        // constructor with an array as parameter
+        this.width = a[0];
+        this.length = a[1];
+        this.height = a[2];
+    }
+
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public int getLength(){
+        return length;
+    }
+
+    public int getVolume(){
+        return getHeight()*getLength()*getWidth();
+    }
+
+    public int getSurfaceArea(){
+        return (2* getWidth()*getLength()) +(2*getLength()*getHeight()) + (2*getHeight()*getWidth());
     }
 }
