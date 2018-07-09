34acb61dfca59dd606ec3db076a6bc234b4c602b
Nampy <at_ben@encs.concordia.ca>
2018-07-08 23:21:20 -0400

    created a constructor and variables to go with the block

---
+++ src/com/company/Block.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Block {
+    private int width;
+    private int height;
+    private int length;
+    public Block(int[] a){
+        // constructor
+        this.width = a[0];
+
+    }
+}
